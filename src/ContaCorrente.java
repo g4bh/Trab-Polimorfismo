8b75567a3747f36345696271b772c37670dba621
Gabu <44330034+g4bh@users.noreply.github.com>
2023-06-17 01:18:29 +0000

    Adicionado duas classes para a aplicação

---
+++ src/ContaCorrente.java
@@ -0,0 +1,20 @@
+
+public class ContaCorrente extends InstrumentoFinanceiro {
+	private float limite;
+	
+	public void setLimite(float limite) {
+		this.limite = limite; 
+	}
+	
+	
+	@Override
+	public float calcularSaldoTotal() {
+		float saldoTotalLimite;
+		
+		saldoTotalLimite = limite + getSaldo();
+		return saldoTotalLimite;
+		
+	}
+	
+	
+}
---
+++ src/FundoDeAplicacao.java
@@ -0,0 +1,15 @@
+
+public class FundoDeAplicacao extends InstrumentoFinanceiro{
+	private float rentabilidade;
+	
+	public void setRentabilidade(float rentabilidade) {
+		this.rentabilidade = rentabilidade;
+	}
+	
+	@Override
+	public float calcularSaldoTotal() {
+		float saldoTotalRentabilidade;
+		saldoTotalRentabilidade = getSaldo() + (getSaldo() * rentabilidade / 100);
+		return saldoTotalRentabilidade; 
+	}
+}
